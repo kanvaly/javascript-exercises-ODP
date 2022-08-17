211000603aad00d35ab44a74532b048213ccae5d
kanvaly <b.kanvaly100@gmail.com>
2022-08-17 23:06:14 +0000

    Exercise done. All the tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,31 @@
-const repeatString = function() {
+const repeatString = function(strng, num) {
+
+    let word = strng; //string to be repeated
+    let factor= parseInt(num);
+
+    //Make sure that the provide string is a correct one
+    if (typeof word !== "string"){
+        return "You must provide a string as first parameter"
+    }
+    
+    // Make sure the provided number is an integer comprised between 1 and 999
+    if (factor < 0 || factor > 999){
+        return "ERROR"
+    }
+
+    //Check for blank strings. Always return " " no matter the string length or value of factor
+   
+
+    //Normal flow of the function
+    let repeatedWord= "";
+
+    while (factor > 0){
+        repeatedWord = repeatedWord + word;
+        factor--;
+    }
+
+
+    return repeatedWord;
 
 };
 

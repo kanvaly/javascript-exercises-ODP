dff9992058a6a3b3e68075b927df0150a65393c9
kanvaly <b.kanvaly100@gmail.com>
2022-08-18 17:52:08 +0000

    Exercise done. All the tests passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+
+    let arry = arr;
+    let toRemov = toRemove;
+    let sorted = [];
+    let j = null;
+
+    for (let i= toRemov.length-1; i>=0; i--){
+
+        j= arry.length-1; //get the length of the new array of sorted elements
+             
+        while (j>=0){ //to walk through the elements of the array
+
+            if (toRemov[i] !== arry[j]){
+                sorted.unshift(arry[j]);
+            }
+
+            j--;
+        }
+
+
+        arry=sorted; //reinitialize the array with the newly sorted elements
+        sorted=[]; //reinitialize sorted to an empty array
+        
+    }
+
+    return arry;
 
 };
 

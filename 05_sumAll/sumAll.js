53d3a11d8a8a4b7bb0b740771ea68b5060c43764
kanvaly <b.kanvaly100@gmail.com>
2022-08-21 04:57:37 +0000

    Exercise done. All the tests passed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    nb1 = int1; nb2 = int2;
+
+    if (nb1 < 0 || nb2 < 0){
+        return "ERROR";
+    }
+
+    else if ((typeof nb1 !== "number") || (typeof int2 !== "number")){
+        return "ERROR"
+    }
+
+    if (nb1 < nb2){
+        let cp = nb2 - nb1; let sum = nb1;
+        while (cp > 0){
+            sum = sum + nb1 + cp;
+            cp--;
+        }
+
+        return sum;
+    }
+
+    else {
+        let cp = nb1 - nb2; sum = nb2;
+        while (cp > 0){
+            sum = sum + nb2 + cp;
+            cp--;
+        }
+
+        return sum;
+    }
+
 
 };
 

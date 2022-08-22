b3b5ba1b0b205f70c8e4f2f2ebeccbf0f96aab17
kanvaly <b.kanvaly100@gmail.com>
2022-08-22 01:17:56 +0000

    Exercise done. All the tests passed!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const ftoc = function() {
+const ftoc = function(far) {
+
+  let temp = far;
+  temp = ((temp - 32) * 5 / 9);
+
+  return Math.round(temp*10)/10; //round to one decimal place. Could use toFixed() but this function returns a sting
 
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+
+  let temp = cel;
+  temp = (temp * 9 / 5 + 32);
+
+
+  return Math.round(temp*10)/10; //round to one decimal place. Could use toFixed() but this function returns a sting
 
 };
 
